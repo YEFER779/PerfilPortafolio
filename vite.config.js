8feb7bb3ba1c3ef992038e9132d5aa6e7c9aedcd
Yefers12 <yefersonlopez243@gmail.com>
2025-02-28 23:11:31 -0600

    Cambio3

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,36 @@
-import path, { resolve } from "node:path";
-import { defineConfig } from 'vite';
-import * as glob from 'glob';
-import htmlPurge from 'vite-plugin-purgecss';
+import path, { resolve } from "node:path"; 
+import { defineConfig } from "vite";
+import * as glob from "glob";
+import htmlPurge from "vite-plugin-purgecss";
 
-const obtenerEntradasHTML = ()=>{
-    return Object.fromEntries(
-        [
-            ...glob
-                .sync('./**/*.html', {ignore: ["./dist/**", "./node_modules/**"]}
-
-            ).map (
-                fileData => [
-                    fileData.slice(0, fileData.length - path.extname(fileData).length),
-                    resolve(__dirname, fileData)
-                ]
-            )
-        ]
-    );
-}
-
+const obtenerEntradasHTML = () => {
+  return Object.fromEntries(
+    [
+      ...glob
+        .sync("./**/*.html", { ignore: ["./dist/**", "./node_modules/**"] })
+        .map((fileData) => [
+          fileData.slice(0, fileData.length - path.extname(fileData).length),
+          resolve(__dirname, fileData),
+        ]),
+    ]
+  );
+};
 
 export default defineConfig({
-    appType: 'mpa',
-    build: {
-        rollupOptions: {
-            input: obtenerEntradasHTML()
-        }
+  base: "./",  // 👈 Agrega esto para corregir rutas en GitHub Pages
+  appType: "mpa",
+  build: {
+    rollupOptions: {
+      input: obtenerEntradasHTML(),
     },
-    plugins: [
-        htmlPurge({})
-    ]
-
+  },
+  plugins: [htmlPurge({})],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
 });
+
